ea14c210e5094c8cb042d0bbdb3ac2637db72dfe
vivshaw <hvivianshaw@gmail.com>
2017-08-02 01:14:40 -0400

    start listing books

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
+import { getAll } from '../utils/BooksAPI'
+import { Card, Image } from 'semantic-ui-react';
 
 
 class App extends Component {
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    getAll().then(books => this.setState({ books }))
+  }
+
   render() {
+    const BookList = this.state.books.map((book) => {
+      return (
+        <Card>
+          <Card.Content>
+            <Image floated='right' size='mini' src={book.imageLinks.thumbnail} />
+            <Card.Header>
+              {book.title}
+            </Card.Header>
+            {book.subtitle &&
+              <Card.Meta>
+                {book.subtitle}
+              </Card.Meta>
+            }
+            <Card.Description>
+              {book.description.substring(0, 140) + "..."}
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            {book.authors.join(', ')}
+          </Card.Content>
+        </Card>
+      )
+    })
+
     return (
       <div className="App">
-        Hello World
+        <Card.Group>{BookList}</Card.Group>
       </div>
     );
   }
