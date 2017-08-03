9c93c50c19beeb637a479ee9e5a85d104727482e
vivshaw <hvivianshaw@gmail.com>
2017-08-03 02:01:46 -0400

    start writings tests for BooksAPI

---
+++ src/utils/__tests__/BooksAPI.test.js
@@ -0,0 +1,29 @@
+import { get, getAll, update, search } from '../BooksAPI';
+
+const example_books = {
+  books: [
+    {
+      title: "Book 1",
+      subtitle: "Subtitle 1",
+      description: "lorem ipsum",
+      authors: ["Guy One", "Guy Two"],
+      imageLinks: { thumbnail: "pic.url" }
+    },
+    {
+      title: "Book 2",
+      subtitle: "Subtitle 2",
+      description: "dolor sit amet",
+      authors: ["Guy Three", "Guy Four"],
+      imageLinks: { thumbnail: "pic2.url" }
+    },
+  ]
+}
+
+const headers = new Headers({"Accept": "application/json", "Content-Type":  "application/json"});
+
+it('fetches correct data', () => {
+  fetch.mockResponse(JSON.stringify(example_books), { headers });
+  getAll().then(returnedBooks => {
+    expect(returnedBooks).toBe(example_books.books)
+  });
+});
