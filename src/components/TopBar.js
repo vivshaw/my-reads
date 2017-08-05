bc248f7b489552b263182561074fc8579510a061
vivshaw <hvivianshaw@gmail.com>
2017-08-05 19:44:22 -0400

    add flow to TopBar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,7 +1,14 @@
+// @flow
+
 import React from 'react';
 import { Icon, Input, Menu } from 'semantic-ui-react';
 
-const TopBar = (props) => {
+const TopBar = (
+  props: {
+    toggleMenu: () => void,
+    updateQuery: (query: string) => void
+  }) => {
+
   return (
     <Menu secondary attached="top">
       <Menu.Item onClick={() => props.toggleMenu()} >
