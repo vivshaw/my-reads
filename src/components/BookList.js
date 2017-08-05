712ecc8ced630feb609f7b1c2f55a0daaf071982
vivshaw <hvivianshaw@gmail.com>
2017-08-05 15:53:33 -0400

    make bookList a functional component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,37 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card } from 'semantic-ui-react';
 
 import Book from './Book';
 
+const BookList = (props) => {
+  const { books } = props;
 
-class BookList extends Component {
-  render() {
-    const { books } = this.props;
+  const FilteredBookElements = books.map((book) => {
+    return (
+      <Book
+        key={book.id}
+        title={book.title}
+        subtitle={book.subtitle}
+        description={book.description}
+        authors={book.authors}
+        coverImageUrl={book.imageLinks.thumbnail}
+      />
+    )
+  })
 
-    const FilteredBookElements = this.props.books.map((book) => {
-      return (
-        <Book
-          key={book.id}
-          title={book.title}
-          subtitle={book.subtitle}
-          description={book.description}
-          authors={book.authors}
-          coverImageUrl={book.imageLinks.thumbnail}
-        />
-      )
-    })
-
-    if(books) {
-      return (
-          <Card.Group>{FilteredBookElements}</Card.Group>
-      );
-    } else {
-      return (
-        <div>
-          Loading...
-        </div>
-      )
-    }
+  if (books) {
+    return (
+        <Card.Group>{FilteredBookElements}</Card.Group>
+    );
+  } else {
+    return (
+      <div>
+        Loading...
+      </div>
+    )
   }
 }
 
