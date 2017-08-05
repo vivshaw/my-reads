f2c9842b2eedcffda4a8afa497eea7508182b319
vivshaw <hvivianshaw@gmail.com>
2017-08-05 19:56:42 -0400

    add flow to Shelf

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,53 +1,60 @@
-import React, { Component } from 'react';
+// @flow
+
+import React from 'react';
 import { Menu, Segment } from 'semantic-ui-react';
 import escapeRegExp from "escape-string-regexp";
 
+import type { BookType } from '../flowTypes'
 import BookList from './BookList';
 
-class Shelf extends Component {
-  render() {
-    const { books, filterQuery, clearQuery } = this.props;
-    let showingBooks;
+const Shelf = (
+  props: {
+    title: string,
+    books: Array<BookType>,
+    filterQuery: string,
+    clearQuery: () => void
+  }) => {
+  const { title, books, filterQuery, clearQuery } = props;
+  let showingBooks;
 
-    if (filterQuery) {
-      const match = new RegExp(escapeRegExp(filterQuery), 'i')
-      showingBooks = books.filter((book) => match.test(book.title) || match.test(book.subtitle))
-    } else {
-      showingBooks = books;
-    }
-
-    return (
-      <Segment basic>
-        <Menu>
-          <Menu.Item
-            name={this.props.title}
-            active={false}
-          />
-
-          {books.length !== showingBooks.length && (
-            <Menu.Item
-              name='notifyShowing'
-              active={false}
-              content={`Now showing ${showingBooks.length} of ${books.length} total.`}
-              position="right"
-            />
-          )}
-          {books.length !== showingBooks.length && (
-            <Menu.Item
-              name='showAll'
-              active={false}
-              onClick={() => clearQuery()}
-              position="right"
-            />
-          )}
-        </Menu>
-
-        <BookList
-          books={showingBooks}
-        />
-      </Segment>
-    )
+  if (filterQuery) {
+    const match = new RegExp(escapeRegExp(filterQuery), 'i')
+    showingBooks = books.filter((book) => match.test(book.title) || match.test(book.subtitle))
+  } else {
+    showingBooks = books;
   }
+
+  return (
+    <Segment basic>
+      <Menu>
+        <Menu.Item
+          name={title}
+          active={false}
+        />
+
+        {books.length !== showingBooks.length && (
+          <Menu.Item
+            name='notifyShowing'
+            active={false}
+            content={`Now showing ${showingBooks.length} of ${books.length} total.`}
+            position="right"
+          />
+        )}
+        {books.length !== showingBooks.length && (
+          <Menu.Item
+            name='showAll'
+            active={false}
+            onClick={() => clearQuery()}
+            position="right"
+          />
+        )}
+      </Menu>
+
+      <BookList
+        books={showingBooks}
+      />
+    </Segment>
+  )
 }
 
 export default Shelf;
