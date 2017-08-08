6a878e3aeef16fe9ad5a3dda7ceab220b4b76f1a
vivshaw <hvivianshaw@gmail.com>
2017-08-08 00:33:55 -0400

    refactor App into Home and Search

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,23 @@
 // @flow
 
 import React, { Component } from 'react';
-import { Icon, Menu, Segment, Sidebar } from 'semantic-ui-react';
-import { Link, Route } from 'react-router-dom';
+import { Segment, Sidebar } from 'semantic-ui-react';
+import { Route } from 'react-router-dom';
 
 import TopBar from './TopBar.js';
-import Shelf from './Shelf';
+import SideMenu from './SideMenu';
+import Home from './Home';
+import Search from './Search';
+
 import { getAll } from '../utils/BooksAPI';
 import type { BookType } from '../common/flowTypes';
 
 class App extends Component {
 	state = {
+		books: [],
 		filterQuery: '',
 		menuVisible: false,
-		shelves: [],
-		shelvedBooks: {}
+		shelves: []
 	};
 
 	toggleMenu = () => {
@@ -32,33 +35,15 @@ class App extends Component {
 	componentDidMount() {
 		getAll().then((books: Array<BookType>) => {
 			const shelves = Array.from(new Set(books.map(book => book.shelf)));
-			const shelvedBooks = shelves.reduce((map, shelf) => {
-				map[shelf] = books.filter(book => book.shelf === shelf);
-				return map;
-			}, {});
 			this.setState({
-				shelves,
-				shelvedBooks
+				books,
+				shelves
 			});
 		});
 	}
 
 	render() {
-		const { shelves, shelvedBooks } = this.state;
-
-		const showingShelves = shelves
-			.filter((shelf: string) => shelvedBooks[shelf])
-			.map(shelf => {
-				return (
-					<Shelf
-						key={shelf + '-id'}
-						title={shelf}
-						books={this.state.shelvedBooks[shelf]}
-						filterQuery={this.state.filterQuery}
-						clearQuery={this.clearQuery}
-					/>
-				);
-			});
+		const { filterQuery, shelves, books } = this.state;
 
 		return (
 			<div className="App">
@@ -69,43 +54,20 @@ class App extends Component {
 				/>
 
 				<Sidebar.Pushable as={Segment} attached="bottom">
-					<Sidebar
-						width="thin"
-						as={Menu}
-						animation="uncover"
-						visible={this.state.menuVisible}
-						icon="labeled"
-						vertical
-						inverted
-					>
-						<Link to="/">
-							<Menu.Item>
-								<Icon name="home" />Home
-							</Menu.Item>
-						</Link>
-						<Link to="/search">
-							<Menu.Item>
-								<Icon name="search" />Search
-							</Menu.Item>
-						</Link>
-					</Sidebar>
+					<SideMenu menuVisible={this.state.menuVisible} />
 
 					<Route
 						exact
 						path="/"
 						render={() =>
-							<Sidebar.Pusher>
-								{showingShelves}
-							</Sidebar.Pusher>}
+							<Home
+								books={books}
+								shelves={shelves}
+								filterQuery={filterQuery}
+								clearQuery={this.clearQuery}
+							/>}
 					/>
-					<Route
-						exact
-						path="/search"
-						render={() =>
-							<Sidebar.Pusher>
-								<div>welcome to the search screen.</div>
-							</Sidebar.Pusher>}
-					/>
+					<Route exact path="/search" render={() => <Search />} />
 				</Sidebar.Pushable>
 			</div>
 		);
