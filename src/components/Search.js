a8c26864670d3d39cb3ee2e78444ae1fd97b43f1
vivshaw <hvivianshaw@gmail.com>
2017-08-08 00:33:19 -0400

    write component for Search page

---
+++ src/components/Search.js
@@ -0,0 +1,14 @@
+// @flow
+
+import React from 'react';
+import { Sidebar } from 'semantic-ui-react';
+
+const Search = () => {
+	return (
+		<Sidebar.Pusher>
+			<div>welcome to the search screen.</div>
+		</Sidebar.Pusher>
+	);
+};
+
+export default Search;
