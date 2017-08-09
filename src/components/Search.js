22baad618ebf005d1e17872bdc0623c63ce0a6f9
vivshaw <hvivianshaw@gmail.com>
2017-08-09 02:43:55 -0400

    write skeleton for Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,55 @@
 // @flow
 
-import React from 'react';
-import { Sidebar } from 'semantic-ui-react';
+import React, { Component } from 'react';
+import { Button, Input, Segment, Sidebar } from 'semantic-ui-react';
 
-const Search = () => {
-	return (
-		<Sidebar.Pusher>
-			<div>welcome to the search screen.</div>
-		</Sidebar.Pusher>
-	);
-};
+import BookList from './BookList';
+import { search } from '../utils/BooksAPI';
+import searchTerms from '../common/searchTerms';
+
+class Search extends Component {
+	state = {
+		searchTerm: '',
+		searchResults: []
+	};
+
+	onSearch = (query: string) => {
+		if (query === '') {
+			// FIXME: toast user
+			console.log('empty query!');
+		} else if (!searchTerms.includes(query)) {
+			// FIXME: toast user, show list of allowed terms?
+			console.log('query not in allowed terms');
+		} else {
+			search(query, 10).then(searchResults => {
+				this.setState({
+					searchResults
+				});
+			});
+		}
+	};
+
+	onChange = searchTerm => {
+		this.setState({ searchTerm });
+	};
+
+	render() {
+		return (
+			<Sidebar.Pusher>
+				<Segment basic>
+					<Input
+						icon="search"
+						value={this.state.searchTerm}
+						onChange={event => this.onChange(event.target.value)}
+					/>
+					<Button onClick={() => this.onSearch(this.state.searchTerm)}>
+						Search
+					</Button>
+					<BookList books={this.state.searchResults} />
+				</Segment>
+			</Sidebar.Pusher>
+		);
+	}
+}
 
 export default Search;
