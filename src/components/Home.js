60a6b55c125c47932cdbc0eeab2aad04338637f5
vivshaw <hvivianshaw@gmail.com>
2017-08-12 21:58:51 -0400

    switch Home to a Tabbed react-swipeable-view

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,41 +1,68 @@
 // @flow
 
-import React from 'react';
-import { Sidebar } from 'semantic-ui-react';
+import React, { Component } from 'react';
+import { Tabs, Tab } from 'material-ui/Tabs';
+import SwipeableViews from 'react-swipeable-views';
 
 import Shelf from './Shelf';
 import type { BookType } from '../common/flowTypes';
 
-const Home = (props: {
-	books: Array<BookType>,
-	shelves: Array<string>,
-	filterQuery: string,
-	clearQuery: () => void
-}) => {
-	const { books, shelves, filterQuery, clearQuery } = props;
+class Home extends Component {
+	props: {
+		books: Array<BookType>,
+		shelves: Array<string>,
+		filterQuery: string,
+		clearQuery: () => void
+	};
 
-	const shelvedBooks = shelves.reduce((map, shelf) => {
-		map[shelf] = books.filter(book => book.shelf === shelf);
-		return map;
-	}, {});
+	state = {
+		slideIndex: 0
+	};
 
-	const showingShelves = shelves.map(shelf => {
+	handleTabChange = (value: number) => {
+		this.setState({
+			slideIndex: value
+		});
+	};
+
+	render() {
+		const { books, shelves, filterQuery, clearQuery } = this.props;
+
+		const shelvedBooks = shelves.reduce((map, shelf) => {
+			map[shelf] = books.filter(book => book.shelf === shelf);
+			return map;
+		}, {});
+
+		const shelfTabs = shelves.map(shelf => {
+			return <Tab label={shelf} value={shelves.indexOf(shelf)} />;
+		});
+
+		const showingShelves = shelves.map(shelf => {
+			return (
+				<Shelf
+					key={shelf + '-id'}
+					title={shelf}
+					books={shelvedBooks[shelf]}
+					filterQuery={filterQuery}
+					clearQuery={clearQuery}
+				/>
+			);
+		});
+
 		return (
-			<Shelf
-				key={shelf + '-id'}
-				title={shelf}
-				books={shelvedBooks[shelf]}
-				filterQuery={filterQuery}
-				clearQuery={clearQuery}
-			/>
+			<div>
+				<Tabs onChange={this.handleTabChange} value={this.state.slideIndex}>
+					{shelfTabs}
+				</Tabs>
+				<SwipeableViews
+					index={this.state.slideIndex}
+					onChangeIndex={this.handleTabChange}
+				>
+					{showingShelves}
+				</SwipeableViews>
+			</div>
 		);
-	});
-
-	return (
-		<Sidebar.Pusher>
-			{showingShelves}
-		</Sidebar.Pusher>
-	);
-};
+	}
+}
 
 export default Home;
