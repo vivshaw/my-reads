8c4a9e564ac2827219e401c05c54309c594d71f8
vivshaw <hvivianshaw@gmail.com>
2017-08-12 21:32:13 -0400

    switch sidebatr to Material UI

---
+++ src/components/SideBar.js
@@ -0,0 +1,35 @@
+// @flow
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import AppBar from 'material-ui/AppBar';
+
+const SideMenu = (props: { menuVisible: boolean }) => {
+	const { menuVisible, handleClose, handleSetVisible } = props;
+
+	return (
+		<Drawer
+			docked={false}
+			width={200}
+			open={menuVisible}
+			onRequestChange={menuVisible => handleSetVisible(menuVisible)}
+		>
+			<AppBar title="flybrary" showMenuIconButton={false} />
+
+			<MenuItem onTouchTap={handleClose} containerElement={<Link to="/" />}>
+				Shelves
+			</MenuItem>
+
+			<MenuItem
+				onTouchTap={handleClose}
+				containerElement={<Link to="/search" />}
+			>
+				Search
+			</MenuItem>
+		</Drawer>
+	);
+};
+
+export default SideMenu;
