914713840821adae60d2cba1c1c378b7032e1628
vivshaw <hvivianshaw@gmail.com>
2017-08-27 21:19:38 -0400

    add jsdoc to getWidth

--- src/utils/getWidth.js
+++ src/utils/getWidth.js
@@ -1,13 +1,25 @@
 // @flow
 
-// MUI's withWidth HOC is great, bbut doesn't support server-side rendering. So,
-// I had to ditch it & write my own mediocre width handler so that React-Snapshot
-// will do its thing.
+/* MUI's withWidth HOC is great, but doesn't support server-side rendering. So,
+   I had to ditch it & write my own mediocre width handler so that React-Snapshot
+   will do its thing. */
 
+// Material-UI components
 import { SMALL, MEDIUM, LARGE } from 'material-ui/utils/withWidth';
 
+/**
+ * Window widths
+ * @type {widths}
+ * @prop {number} 	small 	small width
+ * @prop {number} 	medium 	medium width
+ * @prop {number} 	large 	large width
+ */
 export const widths = { small: SMALL, medium: MEDIUM, large: LARGE };
 
+/**
+ * Gets the window's width.
+ * @return {width} a width from {@link widths}
+ */
 const getWidth = () => {
 	let width;
 	const largeWidth = 992,
