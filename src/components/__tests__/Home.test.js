4e70986f1f937be0c64194993222de58c2dfe695
vivshaw <hvivianshaw@gmail.com>
2017-09-01 00:16:25 -0400

    test tab switching in Home

--- src/components/__tests__/Home.test.js
+++ src/components/__tests__/Home.test.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-
-import { Tabs } from 'material-ui/Tabs';
+import { shallow, mount } from 'enzyme';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 import Home from '../Home';
-import Shelf from '../Shelf';
+
 import { testBooks } from '../../common/testData';
 
 describe('Home', () => {
-	let books, clearQuery, filterQuery, shelves, wrapper;
+	let books, clearQuery, filterQuery, mounted, shelves, wrapper;
 
 	beforeAll(() => {
 		books = testBooks.books;
 		shelves = Array.from(new Set(books.map(book => book.shelf)));
 		filterQuery = '';
 		clearQuery = jest.fn();
+
+		injectTapEventPlugin();
+		const muiTheme = getMuiTheme();
 
 		wrapper = shallow(
 			<Home
@@ -26,9 +29,30 @@ describe('Home', () => {
 				findShelf={clearQuery}
 			/>
 		);
+
+		mounted = mount(
+			<Home
+				books={books}
+				shelves={shelves}
+				filterQuery={filterQuery}
+				clearQuery={clearQuery}
+				handleShelfUpdate={clearQuery}
+				findShelf={clearQuery}
+			/>,
+			{
+				context: { muiTheme },
+				childContextTypes: { muiTheme: React.PropTypes.object }
+			}
+		);
 	});
 
 	it('renders shelf tabs', () => {
 		expect(wrapper.find('div').length).toBe(1);
+	});
+
+	it('changes tabs', () => {
+		expect(mounted.state('slideIndex')).toBe(0);
+		mounted.instance().handleTabChange(1);
+		expect(mounted.state('slideIndex')).toBe(1);
 	});
 });
