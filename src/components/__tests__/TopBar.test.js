207de9335cde2cc9f0eafe0d79585b0e13be3672
vivshaw <hvivianshaw@gmail.com>
2017-09-01 01:59:16 -0400

    add tests for toggleDialog to TopBar

--- src/components/__tests__/TopBar.test.js
+++ src/components/__tests__/TopBar.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import { mount } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 import AppBar from 'material-ui/AppBar';
@@ -10,7 +10,7 @@ import IconButton from 'material-ui/IconButton';
 import TopBar from '../TopBar';
 
 describe('TopBar', () => {
-	let toggleMenu, handleFilterChange, handleFilterClear, wrapper;
+	let mounted, toggleMenu, handleFilterChange, handleFilterClear, wrapper;
 
 	beforeAll(() => {
 		toggleMenu = jest.fn();
@@ -21,7 +21,15 @@ describe('TopBar', () => {
 
 	beforeEach(() => {
 		const muiTheme = getMuiTheme();
-		wrapper = mount(
+		wrapper = shallow(
+			<TopBar
+				filterQuery=""
+				toggleMenu={toggleMenu}
+				handleFilterClear={handleFilterClear}
+				handleFilterChange={handleFilterChange}
+			/>
+		);
+		mounted = mount(
 			<MemoryRouter initialEntries={['/shelves']}>
 				<TopBar
 					filterQuery=""
@@ -38,16 +46,19 @@ describe('TopBar', () => {
 	});
 
 	it('should show an AppBar', () => {
-		expect(wrapper.find(AppBar).length).toBe(1);
+		expect(mounted.find(AppBar).length).toBe(1);
 	});
 
 	it('should show a left and right icon button', () => {
-		expect(wrapper.find(IconButton).length).toBe(2);
+		expect(mounted.find(IconButton).length).toBe(2);
 	});
 
 	describe('ui interaction', () => {
-		// can't get state of children, so can't test toggleDialog's effect on state
-		// because MemoryRouter is root :(
+		it('should toggle the dialog', () => {
+			expect(wrapper.state('dialogOpen')).toBe(false);
+			wrapper.instance().toggleDialog();
+			expect(wrapper.state('dialogOpen')).toBe(true);
+		});
 
 		// disabled because can't get the click to simulate :(
 		xit('should toggle the menu when the menu button is clicked', () => {
