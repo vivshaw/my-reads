5ccc372df41d7c31fd955cb65259c6ac3042aca1
vivshaw <hvivianshaw@gmail.com>
2017-09-01 03:05:41 -0400

    fix getShelfWithWidth tests to match correct implementation

--- src/common/__tests__/commonData.tests.js
+++ src/common/__tests__/commonData.tests.js
@@ -2,8 +2,17 @@ import { shelfData } from '../commonData';
 
 describe('commonData', () => {
 	describe('shelfData', () => {
-		it('getShelfWithWidth returns a shelf', () => {
+		it('returns full shelf name on large screens', () => {
 			const shelf = 'wantToRead';
+			window.innerWidth = 993;
+
+			expect(shelfData.getShelfWithWidth(shelf)).toEqual(shelfData[shelf].wide);
+		});
+
+		it('returns truncated shelf name on small screens', () => {
+			const shelf = 'wantToRead';
+			window.innerWidth = 300;
+
 			expect(shelfData.getShelfWithWidth(shelf)).toEqual(
 				shelfData[shelf].narrow
 			);
